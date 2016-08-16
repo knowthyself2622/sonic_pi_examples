dd5d805e04f03f30cf80bd41dc857f8a80f34ec3
Crystal Allen <Crystal Allen>
2016-08-16 15:03:16 -0400

    initial commit

---
+++ laundry.rb
@@ -0,0 +1,33 @@
+def wear(clothes)
+  sample(:bass_drop_c,rate:14)
+  sleep(2)
+end
+
+def wash(temperature)
+  play (temperature)
+  sleep (1)
+end
+
+def dry(dryer_type)
+  play (dryer_type)
+  sleep (3)
+end
+
+5.times do
+  wear(1)
+  stain =[true,false].choose
+  if stain==true
+    sample(:bass_hard_c)
+    sleep 1
+  else
+    sample(:bd_fat)
+    sleep 1
+  end
+  wash(50)
+  dry(20)
+end
+2.times do
+  wear(14)
+  wash(70)
+  dry(20)
+end
